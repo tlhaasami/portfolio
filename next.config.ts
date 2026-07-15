7d6fe53557446f1eac92a704ea1b69f0a16596ea
tlhaasami <tlhaasami@gmail.com>
2026-07-15 18:38:07 +0500

    configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY 
+  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+  : "";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? repoName : "",
   allowedDevOrigins: ['172.20.161.39'],
 };
 
